63dfdc584ec043d650c458815fcaa1ed10f9245f
DoanDuyLong212 <155629275+DoanDuyLong212@users.noreply.github.com>
2026-06-13 00:33:15 +0700

    Update database_connection.py

--- Data_Retriever/database_connection.py
+++ Data_Retriever/database_connection.py
@@ -1,18 +1,12 @@
 from dotenv import load_dotenv
 import os
-import psycopg
-import time
+from sqlalchemy import create_engine
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 def get_connection():
-    print("⏳ connecting DB...")
-
-    start = time.time()
-    conn = psycopg.connect(DATABASE_URL)
-    print("✅ connected in", time.time() - start, "s")
-
-    return conn
+    return engine
