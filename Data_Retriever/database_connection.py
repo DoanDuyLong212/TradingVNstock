db42ca6e73b11ad2e4af350f697ad3e0e9b56409
DoanDuyLong212 <155629275+DoanDuyLong212@users.noreply.github.com>
2026-06-13 00:26:24 +0700

    Update database_connection.py

--- Data_Retriever/database_connection.py
+++ Data_Retriever/database_connection.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import psycopg
+import time
 
 load_dotenv()
 
@@ -8,4 +9,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def get_connection():
-    return psycopg.connect(DATABASE_URL)
+    print("⏳ connecting DB...")
+
+    start = time.time()
+    conn = psycopg.connect(DATABASE_URL)
+    print("✅ connected in", time.time() - start, "s")
+
+    return conn
