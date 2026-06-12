cc47a426790590b5c64d6fecfaea7fe27ca40ef2
DoanDuyLong212 <155629275+DoanDuyLong212@users.noreply.github.com>
2026-06-13 00:46:54 +0700

    Update database_connection.py

--- Data_Retriever/database_connection.py
+++ Data_Retriever/database_connection.py
@@ -7,9 +7,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=300
+    DATABASE_URL
 )
 
 def get_engine():
